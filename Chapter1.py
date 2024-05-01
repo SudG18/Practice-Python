345673c6244156d5cfe4fbb31c1c4ef04f602e78
Sudheer Guntur <109535753+SudG18@users.noreply.github.com>
2024-05-01 15:40:36 +0530

    Update Chapter1.py

--- Chapter1.py
+++ Chapter1.py
@@ -3,7 +3,7 @@ print(name.split(" ")[0])
 print((name.split(" ")[0])+"*"+(name.split(" ")[1]).lower())
 print(name.lower())
 
-numbers = [1:100]
-for number in numbers:
+numbers = range(1:100)
+for number in range:
   if number%3=0:
   print(number +": is Odd")
