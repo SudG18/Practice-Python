cb8cf48915d4777f0755b83a111954970b083498
Sudheer Guntur <109535753+SudG18@users.noreply.github.com>
2024-05-01 15:36:31 +0530

    Update Chapter1.py
    added number logic

--- Chapter1.py
+++ Chapter1.py
@@ -2,3 +2,8 @@ name = "SUDHEER GUNTUR"
 print(name.split(" ")[0])
 print((name.split(" ")[0])+"*"+(name.split(" ")[1]).lower())
 print(name.lower())
+
+numbers = [1:100]
+for number in numbers:
+  if number%3=0:
+  print(number +": is Odd")
