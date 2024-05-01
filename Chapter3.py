e70d2c85edf467bb363511364675fe150969df65
Sudheer Guntur <109535753+SudG18@users.noreply.github.com>
2024-05-01 14:12:22 +0530

    Create Chapter3.py
    arithmetic functions demo

---
+++ Chapter3.py
@@ -0,0 +1,19 @@
+a = 10
+b = 5
+
+def sum():
+  return(a+b)
+
+def subtract():
+  return(a-b)
+
+def multiplication():
+  return(a*b)
+
+def division():
+  return(a/b)
+
+print(sum)
+print(subtract)
+print(multiplication)
+print(division)
