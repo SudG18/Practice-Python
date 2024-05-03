f1fdff3d4cbb33d44da0bcee59030d3b5f980dc2
Sudheer Guntur <109535753+SudG18@users.noreply.github.com>
2024-05-03 17:00:15 +0530

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,23 @@
+a=10
+b=5
+
+def sum():
+  sum=a+b
+  print(a+b)
+
+def subtract():
+  subtract = a-b
+  print(subtract)
+
+def multiplication():
+  multiplication = a*b
+  print(multiplication)
+
+def division():
+  division = a/b
+  print(division)
+
+sum()
+subtract()
+multiplication()
+division()
